f7d41b456fb4450fedc48fb63b80429cb2b4801d
Lam1ro4eQ <alsajgushev@gmail.com>
2022-02-06 14:55:29 +0300

    todolist v1.2

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -5,7 +5,8 @@ import {Todolist} from "./Todolist";
 let tasks = [
     {id: 1, title: "CSS", isDone: true},
     {id: 2, title: "JS", isDone: true},
-    {id: 2, title: "React", isDone: false}
+    {id: 3, title: "React", isDone: false},
+    {id: 4, title: "Redux", isDone: false}
 ]
 
 
