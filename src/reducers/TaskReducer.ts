c2a9f1c17814166810bedc1c7db7b1a5fa514c65
Lam1ro4eQ <alsajgushev@gmail.com>
2022-07-11 20:19:49 +0300

    todolists

---
+++ src/reducers/TaskReducer.ts
@@ -0,0 +1,24 @@
+import {TaskType} from "../Todolist";
+
+export const TaskReducer = (state: Array<TaskType>, action: tsarACType) => {
+    switch (action.type) {
+        case 'ADD_TODOLIST': {
+            return state
+        }
+        default:
+            return state
+    }
+}
+
+type tsarACType = addtodoListACType
+
+type addtodoListACType = ReturnType<typeof addtodoListAC>
+
+const addtodoListAC = () => {
+    return {
+        type: "ADD_TODOLIST",
+        payload: {
+
+        }
+    } as const
+}
